babbf6b78232a0b1798de72778c5115ac81feb5b
perpouh <perpouh@gmail.com>
2020-10-14 18:34:25 +0900

    処理の分岐とオプション追加のテスト

--- README.md
+++ README.md
@@ -10,7 +10,29 @@ It is used when "I don't need a framework, but I want to use DB and want to cont
 
 ## Usage
 
+```
+pygration create --name="table_name"
+```
 
+`create` generate migration file to create table.
 
-## Contributing
+```
+pygration alter --name="table_name"
+```
 
+`alter` generate migration file to alter table.
+
+```
+pygration upgrade
+```
+
+`upgrade` upgrade database schema.
+
+```
+pygration downgrade [OPTIONS]
+Options:
+  -v, --version INTEGER Roll back to the specified version
+```
+
+`downgrade` downgrade database schema.  
+If the `-v` option is not specified, roll back step to step.
--- src/pygration/command.py
+++ src/pygration/command.py
@@ -4,7 +4,34 @@ import sys
 import click
 
 @click.command()
-@click.option('--name', '-n', default='World')
-def main(name):
-    msg = 'Hello, {name}!'.format(name=name)
-    click.echo(msg)
+@click.argument('function', default="help")
+@click.option('--force/', '-f/', default=False)
+@click.option('--name')
+def main(function, force, name):
+  """
+  FUNCTION:\n
+    create: create database schema.\n
+    upgrade: upgrade database schema.\n
+    downgrade: downgrade database schema.
+  """
+  if function == "help":
+    click.echo(__doc__) # TODO: 出ない、なぜ
+  elif function == "create":
+    create(force)
+  elif function == "upgrade":
+    upgrade()
+  else:
+    click.echo("Hello")
+
+def create(force):
+  # フォルダが存在するか確認
+  # 無ければ作成
+  # ファイル作成
+  click.echo("created")
+
+def upgrade():
+  # フォルダが存在するか確認
+  # 未処理ファイルが存在するか確認
+  # スキーマファイル更新
+  # DB更新
+  click.echo("upgraded")
